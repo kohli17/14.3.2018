a34c00a3de3c66c852fbe1abdaaa57add12426d9
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-03-14 15:39:51 +0530

    Create EXIST.C
    print the number

---
+++ EXIST.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void)
+{
+	int a[3],i,k;
+	scanf("%d",&k);
+	for(i=0;i<3;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<3;i++)
+	{
+		if(a[i]==k)
+		printf("yes");
+	}
+	return 0;
+}
